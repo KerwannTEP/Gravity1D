928113c5299fee39c0c6e854d2b582dfdde7a9a9
KerwannTEP <kerwann_tep@yahoo.fr>
2025-10-29 09:32:19 +0100

    fix harmonic

--- Nbody/src/model/Harmonic.jl
+++ Nbody/src/model/Harmonic.jl
@@ -1,6 +1,6 @@
 function _rho_harmonic(x::Float64)
 
-    a = L
+    a = 3/2 * L
     if (abs(x) <= a)
         return M/(2*a)
     else
@@ -12,7 +12,7 @@ end
 
 function _psi_harmonic(x::Float64)
 
-    a = L
+    a = 3/2 * L
     if (abs(x) <= a)
         return G*M/(2*a) * (x^2 + a^2)
     else
@@ -24,12 +24,12 @@ end
 
 function _F_harmonic(E::Float64)
 
-    a = L
+    a = 3/2 * L
     E0 = G*M*a
     if (0.5*E0 <= E < E0)
         return G*M/(2*pi*a) * 1.0/sqrt(2*(E0-E))
     else
-        return D64_0
+        return 0.0
     end
 
 end
@@ -37,7 +37,7 @@ end
 
 function _M_harmonic(x::Float64)
 
-    a = L
+    a = 3/2 * L
     if (x < -a)
         return 0.0
     elseif (abs(x) <= a)
@@ -51,7 +51,7 @@ end
 
 function _invCDF_harmonic(y::Float64) # y in [0, 1]
 
-    a = L
+    a = 3/2 * L
     return 2*a*y - a
     
 end
@@ -59,7 +59,7 @@ end
 
 function _CDFv_harmonic(v::Float64, x::Float64)
     
-    a = L
+    a = 3/2 * L
     E0 = G*M*a
     omegaSq = G*M/a
     vmax = E0 - omegaSq * x^2
@@ -71,7 +71,7 @@ end
 
 function _invCDFv_harmonic(z::Float64, x::Float64)
     
-    a = L
+    a = 3/2 * L
     E0 = G*M*a
     omegaSq = G*M/a
     vmax = E0 - omegaSq * x^2
